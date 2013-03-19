8161021878b385630752b95ff802658d89fa5397
ruiyang <rui.yang@intel.com>
2013-03-19 17:41:13 +0800

    Separate the CorePluginConsole class && add the export tips

---
+++ sources/net.sf.j2s.core/src/net/sf/j2s/core/utils/MptMessages.java
@@ -0,0 +1,21 @@
+package net.sf.j2s.core.utils;
+
+import org.eclipse.osgi.util.NLS;
+
+public class MptMessages extends NLS {
+	private static final String BUNDLE_NAME = "net.sf.j2s.core.utils.messages"; //$NON-NLS-1$
+	public static String Mayloon_Console_Name;
+	public static String Console_Date_Tag;
+	public static String Console_Data_Project_Tag;
+	public static String Not_found_Mayloon_SDK_Title;
+	public static String Not_found_Mayloon_SDK_Message;
+	public static String Not_found_Mayloon_SDK_Version_File_Message;
+	public static String Not_found_Mayloon_External_File_Message;
+	
+	static {
+		NLS.initializeMessages(BUNDLE_NAME, MptMessages.class);
+	}
+	
+	private MptMessages() {
+	}
+}
