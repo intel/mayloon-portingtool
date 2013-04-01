022692ad830477f05fbdc937693544f68c88590b
ruiyang <rui.yang@intel.com>
2013-04-01 13:07:58 +0800

    enable progress monitor for converting project

--- com.intel.ide.eclipse.mpt/src/com/intel/ide/eclipse/mpt/popup/actions/MayloonConvertAction.java
+++ com.intel.ide.eclipse.mpt/src/com/intel/ide/eclipse/mpt/popup/actions/MayloonConvertAction.java
@@ -7,6 +7,10 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -41,61 +45,84 @@ public class MayloonConvertAction  implements IObjectActionDelegate {
 		if (selection instanceof IStructuredSelection) {
 			for (Iterator it = ((IStructuredSelection) selection).iterator(); it.hasNext();) {
 				Object element = it.next();
-				IProject project = null;
+				final IProject project;
 				
 				if (element instanceof IProject) {
 					project = (IProject) element;
 				} else if (element instanceof IAdaptable) {
 					project = (IProject)((IAdaptable)element).getAdapter(IProject.class);
+				} else{
+					project = null;
 				}
+				
 				if (project != null) {
-					try {
-						if(ProjectUtil.isLibraryProject(project)) {
-							MptPluginConsole.error(MptConstants.CONVERT_TAG, "Convert library project '%1$s' is not supported.", project.getName());
-							return;
-						}
-						
-						if (!ProjectUtil.checkVersionMatch(project)) {
-							return;
-						}
-						
-						// generate .j2s configuration file
-						MayloonPropertiesBuilder.mayloonPropBuild(project);
-						
-						String deployMode = ProjectUtil.getDeployMode(project);
-						
-						ProjectUtil.backupProject(project);
-						
-						// get package name from AndroidManifest.xml
-						// packageName is [packageName], not include the main activity name.(not compatible with android internal implementation)
-						String packageName = ProjectUtil.extractPackageFromManifest(project);
-						
-						// merge j2s class path modify logic to it.
-						ProjectUtil.fixMayloonClassEntry(project);
-						
-						// merge j2s nature to it.
-						MayloonNature.addProjectNature(project);
-						
-						// copy mayloon framework resource and js library
-						ProjectUtil.addMayloonFrameworkFolder(project, deployMode, packageName);
-						
-						// copy android build output resource to /bin/apps/[package name]/
-						ProjectUtil.addAndroidOutput2Mayloon(project, deployMode, packageName, false);
-						
-						// clear android generated gen/ folder
-						ProjectUtil.clearAndroidGenFolder(project);			
-						
-						// TODO luqiang, skip this release
-						// ProjectUtil.addAntBuildSupport(project);	
-						
-						// TODO luqiang, add monitor for it.
-						project.refreshLocal(IResource.DEPTH_INFINITE, null);
-						
-						MptPluginConsole.general(MptConstants.CONVERT_TAG, "Project '%1$s' has been converted successfully.", project.getName());
-					} catch (CoreException e) {
-						MptPluginLogger.throwable(e);
-						MptPluginConsole.error(MptConstants.CONVERT_TAG, "Project '%1$s' could not be converted due to cause {%2$s}", project.getName(), e.getMessage());
+					if(ProjectUtil.isLibraryProject(project)) {
+						MptPluginConsole.error(MptConstants.CONVERT_TAG, "Convert library project '%1$s' is not supported.", project.getName());
+						return;
 					}
+						
+					if (!ProjectUtil.checkVersionMatch(project)) {
+						return;
+					}
+					
+					//do convert and use monitor to show convert progress 
+					Job convertJob = new Job("Convert Project") {		
+						@Override
+						protected IStatus run(IProgressMonitor monitor) {
+
+							// 8 steps to convert project
+							monitor.beginTask("Project converting...", 8);
+							try {
+								// generate .j2s configuration file
+								MayloonPropertiesBuilder.mayloonPropBuild(project);
+								monitor.worked(1);
+								
+								String deployMode = ProjectUtil.getDeployMode(project);
+								
+								ProjectUtil.backupProject(project);
+								monitor.worked(1);
+								
+								// get package name from AndroidManifest.xml
+								// packageName is [packageName], not include the main activity name.(not compatible with android internal implementation)
+								String packageName = ProjectUtil.extractPackageFromManifest(project);
+								
+								// merge j2s class path modify logic to it.
+								ProjectUtil.fixMayloonClassEntry(project);
+								monitor.worked(1);
+								
+								// merge j2s nature to it.
+								MayloonNature.addProjectNature(project);
+								monitor.worked(1);
+								
+								// copy mayloon framework resource and js library
+								ProjectUtil.addMayloonFrameworkFolder(project, deployMode, packageName);
+								monitor.worked(1);
+								
+								// copy android build output resource to /bin/apps/[package name]/
+								ProjectUtil.addAndroidOutput2Mayloon(project, deployMode, packageName, false);
+								monitor.worked(1);
+								
+								// clear android generated gen/ folder
+								ProjectUtil.clearAndroidGenFolder(project);		
+								monitor.worked(1);
+								
+								// TODO luqiang, skip this release
+								// ProjectUtil.addAntBuildSupport(project);	
+								
+								// TODO luqiang, add monitor for it.
+								project.refreshLocal(IResource.DEPTH_INFINITE, null);
+								monitor.worked(1);
+							} catch (CoreException e) {
+								MptPluginLogger.throwable(e);
+								MptPluginConsole.error(MptConstants.CONVERT_TAG, "Project '%1$s' could not be converted due to cause {%2$s}", project.getName(), e.getMessage());
+							}
+							monitor.done();
+							return Status.OK_STATUS;
+						}
+					};
+					convertJob.setUser(true);
+					convertJob.schedule();
+					MptPluginConsole.general(MptConstants.CONVERT_TAG, "Project '%1$s' has been converted successfully.", project.getName());
 				}
 			}
 		}
