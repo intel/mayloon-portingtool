457390954588d5ff6516dd6849cd9d7a9b785b66
Xun Sun <xun.sun@intel.coom>
2013-08-15 14:54:15 +0800

    fix bug 1784 - generate stub methods in super class when child class tries to override them.

---
+++ com.intel.ide.eclipse.mpt/src/com/intel/ide/eclipse/mpt/ast/QuickAssistProcessor.java
@@ -0,0 +1,114 @@
+/**
+ * Code copied from org.eclipse.jdt.ui.
+ */
+package com.intel.ide.eclipse.mpt.ast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.internal.corext.dom.Bindings;
+import org.eclipse.jdt.internal.corext.util.Messages;
+import org.eclipse.jdt.internal.ui.text.correction.ASTResolving;
+import org.eclipse.jdt.internal.ui.text.correction.CorrectionMessages;
+import org.eclipse.jdt.internal.ui.text.correction.proposals.LinkedCorrectionProposal;
+import org.eclipse.jdt.internal.ui.text.correction.proposals.NewDefiningMethodProposal;
+import org.eclipse.jdt.internal.ui.viewsupport.BasicElementLabels;
+import org.eclipse.jdt.ui.text.java.IInvocationContext;
+
+/**
+ * @author xsun12
+ *
+ */
+public class QuickAssistProcessor {
+
+    public static boolean getCreateInSuperClassProposals(IInvocationContext context, ASTNode node, Map<String, Map> proposals) throws CoreException {
+        if (!(node instanceof SimpleName) || !(node.getParent() instanceof MethodDeclaration)) {
+            return false;
+        }
+        MethodDeclaration decl= (MethodDeclaration) node.getParent();
+        if (decl.getName() != node || decl.resolveBinding() == null || Modifier.isPrivate(decl.getModifiers())) {
+            return false;
+        }
+
+        ICompilationUnit cu= context.getCompilationUnit();
+        CompilationUnit astRoot= context.getASTRoot();
+
+        IMethodBinding binding= decl.resolveBinding();
+        ITypeBinding[] paramTypes= binding.getParameterTypes();
+
+        ITypeBinding[] superTypes= getAllSuperTypes(binding.getDeclaringClass());
+
+        List<SingleVariableDeclaration> params= decl.parameters();
+        String[] paramNames= new String[paramTypes.length];
+        for (int i = 0; i < params.size(); i++) {
+            SingleVariableDeclaration param= params.get(i);
+            paramNames[i]= param.getName().getIdentifier();
+        }
+
+        for (int i= 0; i < superTypes.length; i++) {
+            ITypeBinding curr= superTypes[i];
+            if (curr.isFromSource()) {
+                IMethodBinding method= Bindings.findOverriddenMethodInType(curr, binding);
+                if (method == null) {
+                    ITypeBinding typeDecl= curr.getTypeDeclaration();
+                    ICompilationUnit targetCU= ASTResolving.findCompilationUnitForBinding(cu, astRoot, typeDecl);
+                    if (targetCU != null) {
+                        String fqName = targetCU.findPrimaryType().getFullyQualifiedName();
+                        Map<String, LinkedCorrectionProposal> proposalsMap = proposals.get(fqName);
+                        
+                        if (proposalsMap == null){
+                             continue;
+                        }
+                        String label= Messages.format(CorrectionMessages.QuickAssistProcessor_createmethodinsuper_description, new String[] { BasicElementLabels.getJavaElementName(curr.getName()), BasicElementLabels.getJavaElementName(binding.getName()) });
+                        proposalsMap.put(binding.getName(), new NewDefiningMethodProposal(label, targetCU, astRoot, typeDecl, binding, paramNames, IProposalRelevance.CREATE_METHOD_IN_SUPER));
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
+
+    /**
+     * Returns all super types (classes and interfaces) for the given type in inheritance order.
+     * @param type The type to get the supertypes of.
+     * @return all super types (excluding <code>type</code>)
+     */
+    public static ITypeBinding[] getAllSuperTypes(ITypeBinding type) {
+        ArrayList<ITypeBinding> result= new ArrayList<ITypeBinding>();
+
+        collectSuperTypes(type, result);
+        
+        for(int i=0; i< result.size(); i++){
+            type = result.get(i);
+            collectSuperTypes(type, result);
+        }
+
+//        result.remove(type);
+        return result.toArray(new ITypeBinding[result.size()]);
+    }
+
+    private static void collectSuperTypes(ITypeBinding curr, ArrayList<ITypeBinding> list) {
+        ITypeBinding superClass= curr.getSuperclass();
+        if (superClass != null) {
+            list.add(superClass);
+        }
+        
+        ITypeBinding[] interfaces = curr.getInterfaces();
+        for (int i = 0; i < interfaces.length; i++) {
+            list.add(interfaces[i]);
+        }
+    }
+
+}
