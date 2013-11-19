548b7cc7bda15b412bb696b4e8948ace00e6eced
ruiyang <rui.yang@intel.com>
2013-11-19 14:37:17 +0800

    delete unused projects from MPT dir

--- package/j2s-2.0.0-20100601-eclipse-3.6/plugins/net.sf.j2s.lib_2.0.0/j2slib/org/eclipse/core/internal/commands/util/Assert.js
+++
@@ -1,2 +0,0 @@
-﻿Clazz.declarePackage ("org.eclipse.core.internal.commands.util");
-c$ = Clazz.declareType (org.eclipse.core.internal.commands.util, "Assert");
--- package/j2s-2.0.0-20100601-eclipse-3.6/plugins/net.sf.j2s.lib_2.0.0/j2slib/org/eclipse/core/internal/runtime/Assert.js
+++
@@ -1,2 +0,0 @@
-﻿Clazz.declarePackage ("org.eclipse.core.internal.runtime");
-c$ = Clazz.declareType (org.eclipse.core.internal.runtime, "Assert");
